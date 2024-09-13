016dbe57ddeb9d39c3ff51ab93429497512763c3
Valentyn Slivko <metallistvalon@gmail.com>
2024-09-13 22:57:37 +0300

    Fix: adjust lambda invocation func

--- image_proj/image_app/http.py
+++ image_proj/image_app/http.py
@@ -1,14 +1,19 @@
+from typing import Literal
 import requests
 import os
 
 BASE_API_GW_URL = os.environ["API_GW_BASE_URL"]
 
 
-def invoke_image_processing(b64_image: str) -> requests.Response:
+def invoke_image_processing(
+    b64_image: str,
+) -> dict[Literal["status"] | Literal["body"], dict]:
     resp = requests.post(
-        BASE_API_GW_URL + "/myresource",
+        BASE_API_GW_URL + "/image",
         data={"image": b64_image},
         headers={"Content-Type": "application/json", "Accept": "image/png"},
     )
     if resp.status_code == 200:
-        return resp.json()
+        return {"status": resp.status_code, "body": resp.json()}
+
+    return {"status": resp.status_code, "body": resp.json()}
